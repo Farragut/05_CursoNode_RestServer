4c48def1178c92b8d826ceb08381a88e17a3a05b
Farragut <mfnavarrete3@gmail.com>
2018-08-26 16:00:20 -0500

    correcion de server 01

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/usuario', function(req, res) {
     res.send('Get usuario')
