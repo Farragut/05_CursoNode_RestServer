e8eeafafe531e3a2304507da0467fe1bd81acaac
Farragut <mfnavarrete3@gmail.com>
2018-08-29 20:17:35 -0500

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,25 @@
 process.env.PORT = process.env.PORT || 3000;
+
+const USER_MONGODB = 'root';
+const PASWD_MONGODB = 'Semperfi1';
+const PORT_MONGO = 27017;
+const BASE_NAME = 'cafe';
+const CONEX_MONGODB_LOCAL = `mongodb://localhost:${PORT_MONGO}/${BASE_NAME}`;
+const CONEX_MONGODB_MLAB = `mongodb://${USER_MONGODB}:${PASWD_MONGODB}@ds137812.mlab.com:37812/${BASE_NAME}`;
+
+//#######################
+//###     ENTORNO     ###
+//#######################
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//#######################
+//###  BASE DE DATOS  ###
+//#######################
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = CONEX_MONGODB_LOCAL;
+} else {
+    urlDB = CONEX_MONGODB_MLAB;
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -2,42 +2,18 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+var colors = require('colors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.send('Get usuario')
-})
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.send('Delete usuario')
-})
-
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE'.green);
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Escuchando el puerto ${process.env.PORT}`);
+    console.log(`Escuchando el puerto ${process.env.PORT}`.red);
 })
